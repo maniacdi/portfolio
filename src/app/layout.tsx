9198e42ef5c9410e7ff5a0f0eb5e2fa5e6422099
magaldiAMS <javiergmag@ext.inditex.com>
2026-06-19 12:10:50 +0200

    fix: improve responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { JetBrains_Mono,Orbitron, Space_Grotesk } from "next/font/google";
 import { getLocale } from "next-intl/server";
 
 import "../styles/globals.scss";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+};
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
